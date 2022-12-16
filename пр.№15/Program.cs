f3a9863622b029bf3d2e460118eb39d5f6569ca2
user <user@LAPTOP-VCVPT2J9>
2022-12-16 14:39:03 +0300

    Добавьте файлы проекта.

---
+++ пр.№15/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+namespace _82
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var src = "aapabbppccc";
+            var toRemove = 'p';
+            var res = RemoveAt(src, toRemove);
+            Console.WriteLine(src);
+            Console.WriteLine(toRemove);
+            Console.WriteLine(res);
+        }
+        static string RemoveAt(string src, char c)
+        {
+            var sb = new StringBuilder();
+            foreach (var item in src)
+            {
+                if (!item.Equals(c))
+                {
+                    sb.Append(item);
+                }
+            }
+            return sb.ToString();
+        
+        }
+
+    }
+}
